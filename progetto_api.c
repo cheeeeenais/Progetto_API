2117d2f8bd9ad206f891d1395cd4e1a74d2ccabc
chen <peizhou.chen@mail.polimi.it>
2021-05-28 15:47:47 +0200

    first serious commit, taking the first input

--- progetto_api.c
+++ progetto_api.c
@@ -7,5 +7,32 @@
 #include <string.h>
 
 int main() {
-    printf("third commit");
+
+    char first[500], *num1 = NULL, *num2 = NULL, command[500];
+    int d, k;
+
+    fgets(first, 500, stdin);
+    //memset(num1, 0, 50);
+    //memset(num2, 0, 50);
+
+    int space_pos = 0;
+    while (first[space_pos] != ' ') {
+        space_pos++;
+    };
+
+    // space_pos is at the position of the space_pos
+    // first[space_pos] is the space_pos
+    printf("%d", space_pos);
+    printf("\n%c", first[space_pos]);
+
+    // TODO fix
+    //d = atoi(strncpy(num1, first, space_pos - 1));
+    //k = atoi(strncpy(num2, first + space_pos, strlen(first) - space_pos - 2));
+
+    fgets(command, 500, stdin);
+    while (command[0] != 'T') {
+        unsigned long weight;
+        printf("\nciao");
+        fgets(command, 500, stdin);
+    }
 }
